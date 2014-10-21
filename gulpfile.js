5fef2440117639ca6e76fd6335ae3c0457e7f134
jos <mail@jos.tw>
2014-10-21 18:46:41 +0800

    Initial gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/*
+ * ajax-seo-starter-kit
+ *
+ * https://github.com/jostw/ajax-seo-starter-kit
+ *
+ * Copyright (c) 2014 jos
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+var gulp =    require("gulp");
+
+gulp.task("default", function() {
+
+});
