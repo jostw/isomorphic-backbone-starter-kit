9849f4e7afb921b1e3bdbc999aeb1d1ac811b94b
jos <mail@jos.tw>
2014-10-31 00:08:13 +0800

    Add: nodemon and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,52 @@
 "use strict";
 
 var gulp =    require("gulp"),
-    wiredep = require("wiredep").stream;
+    wiredep = require("wiredep").stream,
+
+    plugins = require("gulp-load-plugins")(),
+
+    paths = {
+        html: {
+            src:   "views/**/*.hbs",
+            index: "views/layouts/index.hbs"
+        },
+
+        js: {
+            app: "app.js"
+        }
+    };
 
 gulp.task("html", function() {
-    return gulp.src("views/layouts/index.hbs")
+    return gulp.src([paths.html.src, "!"+ paths.html.index])
+               .pipe(plugins.livereload());
+});
+
+gulp.task("index", function() {
+    return gulp.src(paths.html.index)
                .pipe(wiredep())
-               .pipe(gulp.dest("views/layouts/"));
+               .pipe(gulp.dest("views/layouts/"))
+               .pipe(plugins.livereload());
 });
 
-gulp.task("watch", function() {
-    gulp.watch("views/**/*.hbs", ["html"]);
+gulp.task("reload", function() {
+    return gulp.src(paths.js.app)
+               .pipe(plugins.livereload());
 });
 
-gulp.task("default", ["html", "watch"]);
+gulp.task("watch", ["reload"], function() {
+    gulp.watch([paths.html.src, "!"+ paths.html.index], ["html"]);
+    gulp.watch(paths.html.index, ["index"]);
+});
+
+gulp.task("nodemon", function() {
+    plugins.livereload.listen();
+
+    plugins.nodemon({
+        script: paths.js.app,
+        ext: "js",
+        ignore: ["gulpfile.js"]
+    })
+    .on("start", ["watch"]);
+});
+
+gulp.task("default", ["nodemon"]);
